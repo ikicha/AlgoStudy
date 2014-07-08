10daf5f6ae0e6206f22ce75544262c38a6b27876
Jeongik Cha <ikicha90@gmail.com>
2014-07-08 22:51:06 +0900

    To CPP, but in progress......

--- Week01/3.FENCE/ikicha/fence.cpp
+++ Week01/3.FENCE/ikicha/fence.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#define BIGGER( x , y ) ((x) > (y) ? (x) : (y))
+#define SMALLER( x , y ) ((x) < (y) ? (x) : (y))
 using namespace std;
 
 long solve();
@@ -15,18 +16,29 @@ inline long solve() {
     int n;
     cin >> n;
     int* data = new int[n];
-    for(int i = 0; i < n; i++)
-        cin >> data[i];
-
-    long result = 0;
+    int max_height = 0, min_height = 10001;
     for(int i = 0; i < n; i++) {
-        int min = 10001;
-        for(int j = i; j < n; j++) {
-            min = min < data[j]? min: data[j];
-            int size = (j-i+1) * min;
-            result = result > size? result: size;
-        }
+        cin >> data[i];
+        max_height = BIGGER(data[i],max_height);
+        min_height = SMALLER(data[i],min_height);
     }
+    long result = 0;
+    int* temp = new int[max_height+1];
+    for(int i = min_height; i < max_height; i++)
+        temp[i] = 0;
+    for(int i = 0; i < n; i++)
+        for(int j = min_height; j <= max_height; j++)
+            if(j <= data[i]) {
+                temp[j]++;
+                result = BIGGER(temp[j] * j, result);
+            } else {
+                result = BIGGER(temp[j] * j, result);
+                temp[j]=0;
+            }
+    for(int i = min_height; i < max_height; i++)
+        result = BIGGER(temp[i]*i, result);
+
+    delete[] temp;
     delete[] data;
     return result;
-} 
+}
