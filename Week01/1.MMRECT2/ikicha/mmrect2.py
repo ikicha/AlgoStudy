ad60da0eafe1e30bc25817ea0a786b75e9e663f3
jeongik.cha <ikicha90@gmail.com>
2014-07-12 18:26:15 +0900

    [Prototype][Python]Map from Array => Stiil timeout, i should use cpp.

--- Week01/1.MMRECT2/ikicha/mmrect2.py
+++ Week01/1.MMRECT2/ikicha/mmrect2.py
@@ -1,25 +1,31 @@
-def solve():
+def solve():#f):
     n = input()
-    data = []
+#    n = eval(f.readline())
+    data = {}
     for i in range(0, n):
-        in_var = raw_input()
-        data.append(map(eval, in_var.split(" ")))
+        temp = map(eval, raw_input().split(" "))
+#        temp = map(eval, f.readline().split())
+        if not temp[0] in data.keys():
+            data[temp[0]] = []
 
-    data.sort()
+        data[temp[0]].append(temp[1])
+
     min_result = -1
     max_result = 0
-    for i in range(0, len(data)):
-        for j in range(i + 1, len(data)):
-            if data[i][0]!=data[j][0]:
-                break
-            size = abs(data[i][1] - data[j][1])
-            if ([data[i][0]+size, data[i][1]] in data and [data[j][0]+size, data[j][1]] in data):
-                max_result = max(max_result, size)
-                min_result = min_flag(min_result, size)
+    for points in data.keys():
+        data_point = data[points]
+        for left in range(0, len(data_point)):
+            for right in range(left + 1, len(data_point)):
+                size = abs(data_point[left] - data_point[right])
+                if points + size in data:
+                    if data_point[left] in data[points + size] and data_point[right] in data[points + size]:
+                        max_result = max(max_result, size)
+                        min_result = min_flag(min_result, size)
 
-            if ([data[i][0]-size, data[i][1]] in data and [data[j][0]-size, data[j][1]] in data):
-                max_result = max(max_result, size)
-                min_result = min_flag(min_result, size)
+                if points - size in data:
+                    if data_point[left] in data[points - size] and data_point[right] in data[points - size]:
+                        max_result = max(max_result, size)
+                        min_result = min_flag(min_result, size)
         
     print str(min_result) + " " + str(max_result)
 
@@ -28,14 +34,15 @@ def min_flag(x, y):
         return y
     else:
         return min(x,y)
-
                
 
 
 
-
+#f = open("input.txt")
 num_case = input()
+#num_case = eval(f.readline())
 
 for c in range(0, num_case):
+    #solve(f)
     solve()
-
+#f.close()
