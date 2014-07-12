3f6b1bfe035612170fee7849f5690f02f89a5cce
jeongik.cha <ikicha90@gmail.com>
2014-07-12 20:31:32 +0900

    [Week1][Problem1][PASS] CPP zzang

--- Week01/1.MMRECT2/ikicha/mmrect2.cpp
+++ Week01/1.MMRECT2/ikicha/mmrect2.cpp
@@ -1,33 +1,70 @@
+#include <map>
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <fstream>
+//#include <fstream>
+#include <set>
+#include <cmath>
+#define CONTAINS( x , y ) ((x).find(y) != (x).end())
+
 using namespace std;
 
-bool compareByX(const pair<int, int> lhs, const pair<int, int> rhs) {
-    return lhs.first < rhs.first;
-}
-
+inline void solve(ifstream &fin);
+inline void solve();
+inline int min_flag(int x, int y);
 int main() {
     int num_case;
-    ifstream fin("input.txt");
-    fin>> num_case;
-    for(int c = 0; c < num_case; c++) {
-        int num;
-        fin >> num;
-        vector< pair <int, int> > data;
-        for(int i = 0; i < num; i++) {
-            int temp1, temp2;    
-            fin >> temp1 >> temp2;
-            data.push_back(make_pair<int, int>(temp1, temp2));
-        }
-        sort(data.begin(), data.end(), compareByX);
-
-        vector<pair<int, int> >::iterator it = data.begin(), end = data.end();
-
-        for(;it!=end;++it)
-            cout << "(" << it->first << ", " << it->second << ")" << endl;
+    //ifstream fin("input.txt");
+    //fin>> num_case;
+    cin>> num_case;
+    for(int c = 0; c < num_case; c++) { 
+        //solve(fin);
+        solve();
     }
-
+    // fin.close();
     return 0;
+}
+
+
+//inline void solve(ifstream &fin) {
+inline void solve() {
+    map<int, set<int> > data;
+    int num;
+    //fin >> num;
+    cin >> num;
+    for(int i = 0; i < num; i++) {
+        int temp1, temp2;    
+        //fin >> temp1 >> temp2;
+        cin >> temp1 >> temp2;
+        data[temp1].insert(temp2);
+    }
+        
+    map<int, set<int> >::iterator i;
+    set<int>::iterator j, k;
+    int max_result = 0, min_result = 0;
+    for(i = data.begin(); i!=data.end();++i) {
+        for(j = i->second.begin(); j!=i->second.end()--; ++j) {
+            for(k = j; k!=i->second.end();++k) {
+                if(*j != *k){
+                   int size = abs(*j - *k);
+                    for(int l = -1; l<2; l=l+2){
+                        int flag = i->first + size * l;
+                        if(CONTAINS(data, flag)) {
+                            if(CONTAINS(data[flag], *j) && CONTAINS(data[flag], *k)) {
+                                max_result = max(max_result, size);
+                                min_result = min_flag(min_result, size);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        
+    }
+    cout << min_result << " " << max_result << endl;
+}
+
+inline int min_flag(int x, int y) {
+    return x==0? y: min(x,y);
 }
